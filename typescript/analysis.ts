beeb8bedd81f3f4713547143bb41e17f340fcb7d
Christopher Wells <cwellsny@nycap.rr.com>
2022-01-09 22:29:40 -0800

    Create initial implementation of solve method

--- typescript/analysis.ts
+++ typescript/analysis.ts
@@ -11,7 +11,7 @@ class ChoiceOption {
     }
 
     static fromObject(optionObject: any): ChoiceOption {
-        return new ChoiceOption(optionObject["text"], optionObject["effect"]);
+        return new ChoiceOption(optionObject["text"], parseInt(optionObject["effect"]));
     }
 }
 
@@ -38,6 +38,60 @@ class Choice {
     }
 }
 
+class Problem {
+    choices: Choice[];
+    chosen: [string, string][];
+
+    constructor(choices: Choice[], chosen: [string, string][]) {
+        this.choices = choices;
+        this.chosen = chosen;
+    }
+
+    solve(): Map<number, [string, string][]> {
+        let possible_values: Map<number, [string, string][]> = new Map<number, [string, string][]>();
+        possible_values.set(0, [...this.chosen]);
+
+        let chosen = [...this.chosen];
+
+        for (let choice of this.choices) {
+            if (Problem.alreadyMadeChoice(choice, chosen)) {
+                continue;
+            }
+
+            let next_possible_values: Map<number, [string, string][]> = new Map<number, [string, string][]>();
+
+            for (let [score, past_choices] of possible_values.entries()) {
+                let options = [...choice.options];
+                options.reverse(); // Reverse so that we tend to choose the first option
+
+                for (let option of options) {
+                    let new_score = score + option.effect;
+                    let new_choices = [...past_choices];
+                    new_choices.push([choice.text, option.text]);
+
+                    next_possible_values.set(new_score, new_choices);
+                }
+            }
+
+            possible_values = next_possible_values;
+        }
+
+        return possible_values;
+    }
+
+    static alreadyMadeChoice(choice: Choice, chosen: [string, string][]): Boolean {
+        for (let j in chosen) {
+            let c = chosen[j];
+
+            if (c[0] == choice.text) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
 function convertChoices(choices: any[]) {
     let converted = [];
 
@@ -53,3 +107,9 @@ function convertChoices(choices: any[]) {
 
 console.log(choices);
 console.log(convertChoices(choices));
+
+let problem = new Problem(convertChoices(choices), []);
+console.log(problem.solve());
+
+problem = new Problem(convertChoices(choices), [["\"Present to me your left arm.\"", "Extend your arm."]]);
+console.log(problem.solve());
